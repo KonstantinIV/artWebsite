e9c6f661381c3e77b90c39e4e5a57f3b0733598b
Konsta tin <konstantin-r@hotmail.com>
2023-05-29 19:42:03 +0300

    removed bootrstrap css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
 
 
 
-  <!-- BOOTSTRAP -->
+  <!-- BOOTSTRAP 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" 
    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" 
    crossorigin="anonymous">
@@ -32,7 +32,7 @@
    crossorigin="anonymous"
    onerror="this.onerror=null;this.href='css/bootstrap.min.css';" 
    />
-   
+   -->
 
 
 
