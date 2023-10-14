725206f959d8965327cab7dfd611e224f16aa512
Konsta tin <konstantin-r@hotmail.com>
2023-10-14 15:31:35 +0300

    seo changes

--- resources/js/components/pages/home/aboutCard/aboutCardText.jsx
+++ resources/js/components/pages/home/aboutCard/aboutCardText.jsx
@@ -7,19 +7,23 @@ export default function AboutCardText() {
     const email = "kosta.artist" + "@" + "outlook.com";
 
     const artistDescription = useContext(MyContext);
-    
-    return (
-        <div className="aboutTextContainer">
-            <h3 className="aboutTextHeadline" >
-                About
-            </h3>
-            <p className="aboutText" dangerouslySetInnerHTML={{ __html: artistDescription }}>
 
-            </p>
-            <br />
-            <div className="aboutContact">
-                <p className="aboutContacGreen">Contact</p> <span>:</span>   <p>{email}</p>
-            </div>
-        </div>
+    return (
+        <main className="aboutTextContainer">
+            <article>
+
+                <h1 className="aboutTextHeadline" >
+                    About
+                </h1>
+                <p className="aboutText" dangerouslySetInnerHTML={{ __html: artistDescription }}>
+
+                </p>
+                <br />
+                <div className="aboutContact">
+                    <p className="aboutContacGreen">Contact</p> <span>:</span>   <p>{email}</p>
+                </div>
+            </article>
+
+        </main>
     )
 }
