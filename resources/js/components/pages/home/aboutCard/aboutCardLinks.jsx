7323ae213c4543fb6b92c849c220544ff3b5c429
Konsta tin <konstantin-r@hotmail.com>
2024-12-15 11:05:25 +0200

    about card changes

--- resources/js/components/pages/home/aboutCard/aboutCardLinks.jsx
+++ resources/js/components/pages/home/aboutCard/aboutCardLinks.jsx
@@ -17,9 +17,13 @@ export default function AboutCardLinks() {
                 Instagram
             </a>
 
-            <a className='aboutLink' href='https://www.facebook.com/rosenhartartist' target='_blank'>
+            <a className='aboutLink disabledAboutLink' href='https://www.facebook.com/rosenhartartist' target='_blank'>
                 <img loading="lazy" src='img/icons/social-media/facebook.png' className='aboutLinkIcon' alt='facebook icon' fill='currentColor' width="16" height="16" />
                 Facebook
+            </a>
+            <a  className=' aboutLink' href='https://bsky.app/profile/rosenhart.bsky.social' target='_blank'>
+                <img loading="lazy" src='img/icons/social-media/Bluesky.png' className='aboutLinkIcon' alt='artstation icon' fill='currentColor' width="16" height="16" />
+                Bluesky
             </a>
 
             <a className='aboutLink' href='https://twitter.com/Rosenhart_art' target='_blank'>
@@ -32,10 +36,15 @@ export default function AboutCardLinks() {
                 <img loading="lazy" src='img/icons/social-media/artstation.png' className='aboutLinkIcon' alt='artstation icon'  width="16" height="16" />
                 Artstation
             </a>
-            <a  className=' aboutLink' href='https://bsky.app/profile/rosenhart.bsky.social' target='_blank'>
-                <img loading="lazy" src='img/icons/social-media/Bluesky.png' className='aboutLinkIcon' alt='artstation icon' fill='currentColor' width="16" height="16" />
-                Bluesky
+            
+            
+
+            <a  className=' aboutLink' href='https://cara.app/rosenhart' target='_blank'>
+                <img loading="lazy" src='img/icons/social-media/cara.png' className='aboutLinkIcon' alt='cara icon' fill='currentColor' width="16" height="16" />
+                Cara
             </a>
+            <br />
+
             <a  className=' aboutLink' href='https://www.pinterest.com/Rosenhart_artist/' target='_blank'>
                 <img loading="lazy" src='img/icons/social-media/pinterest.png' className='aboutLinkIcon' alt='pinterest icon' fill='currentColor' width="16" height="16" />
                 Pinterest
@@ -45,11 +54,6 @@ export default function AboutCardLinks() {
             <a  className=' aboutLink' href='https://www.tumblr.com/rosenhart-artist' target='_blank'>
                 <img loading="lazy" src='img/icons/social-media/tumblr.png' className='aboutLinkIcon' alt='tumblr icon' fill='currentColor' width="16" height="16" />
                 Tumblr
-            </a>
-
-            <a  className=' aboutLink' href='https://cara.app/rosenhart' target='_blank'>
-                <img loading="lazy" src='img/icons/social-media/cara.png' className='aboutLinkIcon' alt='cara icon' fill='currentColor' width="16" height="16" />
-                Cara
             </a>
          
  
